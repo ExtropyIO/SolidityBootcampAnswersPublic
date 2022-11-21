1a1cb0b184cce4fdbfa1a5b5e658dd9a0c7ecf38
Will Schiller <62856554+WillSchiller@users.noreply.github.com>
2022-11-21 18:56:40 +0000

    Update VolcanoNFT.sol

--- Homework9/src/VolcanoNFT.sol
+++ Homework9/src/VolcanoNFT.sol
@@ -1,9 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-
-// forge install OpenZeppelin/openzeppelin-contracts
-// Remember to update remappings.txt
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
@@ -12,15 +9,12 @@ contract VolcanoNFT is ERC721 {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    constructor() ERC721("VolcanoNFT", "VCN") {
+    constructor() ERC721("VolcanoNFT", "VLC") {}
 
+    function mintNFT(address recipient) public returns (uint256) {
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+        _mint(recipient, newItemId);
+        return newItemId;
     }
-
-    function mint() public returns (uint256) {
-    _tokenIds.increment();
-    uint256 newItemId = _tokenIds.current();
-    _mint(msg.sender, newItemId);
-    return newItemId;
-    }
-   
 }
